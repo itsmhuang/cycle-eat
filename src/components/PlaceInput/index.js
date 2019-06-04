b2804bf2bc62da55adb8a79ae3f85fbcbb508ac6
Michelle Huang <huangmhuang@gmail.com>
2019-06-04 15:01:57 -0700

    fix adding pac-containers on hot update

--- src/components/PlaceInput/index.js
+++ src/components/PlaceInput/index.js
@@ -4,19 +4,15 @@ import { Formik } from 'formik';
 
 const PlaceInput = ({ query, onSetQuery, onFormSubmit }) => {
   const [autocomplete, setAutocomplete] = useState('');
-  const [inputVal, setInputVal] = useState('');
   const inputEl = useRef(null);
-  let setFieldValueFunc = null;
+  let setField = null;
+  let googleDropdown = document.getElementsByClassName('pac-container');
 
   const onSelect = () => {
     // console.log( 'inputEl: ', inputEl.current.value );
     onSetQuery(inputEl.current.value);
     let places = autocomplete.getPlaces();
-    // console.log('places: ', places);
-    setFieldValueFunc(
-      'searchQuery',
-      autocomplete.getPlaces().formatted_address,
-    );
+    setField('searchQuery', autocomplete.getPlaces().formatted_address);
   };
 
   const handleFormSubmit = event => {
@@ -28,24 +24,29 @@ const PlaceInput = ({ query, onSetQuery, onFormSubmit }) => {
     setAutocomplete(new window.google.maps.places.SearchBox(inputEl.current));
   };
 
-  //load script
+  //add script
   useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${
+      process.env.REACT_APP_GOOGLE_API_KEY
+    }&libraries=places`;
+    const headScript = document.getElementsByTagName(`script`)[0];
     if (!window.google) {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${
-        process.env.REACT_APP_GOOGLE_API_KEY
-      }&libraries=places`;
-      const headScript = document.getElementsByTagName(`script`)[0];
       headScript.parentNode.insertBefore(script, headScript);
       script.addEventListener('load', onScriptLoad);
-      return () => script.removeEventListener('load', onScriptLoad);
     } else {
+      if (googleDropdown.length) {
+        googleDropdown[0].remove();
+      }
       onScriptLoad();
     }
+    return () => {
+      script.removeEventListener('load', onScriptLoad);
+    };
   }, [window.google]);
 
-  //add event listener for searchBox
+  //add listener for searchBox
   useEffect(() => {
     if (autocomplete) {
       autocomplete.addListener('places_changed', onSelect);
@@ -64,20 +65,27 @@ const PlaceInput = ({ query, onSetQuery, onFormSubmit }) => {
         setFieldValue,
         // setSubmitting,
       }) => {
-        setFieldValueFunc = setFieldValue;
+        setField = setFieldValue;
         return (
           <Styled.SearchBarForm onSubmit={handleFormSubmit}>
             <Styled.SearchInput
               type="text"
               ref={inputEl}
               name="searchQuery"
-              // value={inputVal}
               placeholder=""
               onKeyDown={event => {
-                // prevent google autofill from submitting form, side effect: can't submit via enter while focused on this field
-                if (event.keyCode === 13) {
-                  // event.preventDefault();
+                /*console.log(
+                  'googleDropdown: ',
+                  googleDropdown[googleDropdown.length - 1],
+                );*/
+                if (
+                  /*googleDropdown[0].style.display === 'none' &&*/
+                  event.keyCode === 13
+                ) {
+                  // console.log( 'googleDropdown[0].style.display: ', googleDropdown[0].style.display );
+                  event.preventDefault();
                 }
+                // prevent google autofill from submitting form, side effect: can't submit via enter while focused on this field
               }}
             />
             <Styled.SearchBtn type="submit">
